e22b986d4bb8636fd99f2fab08d16cbe72d2d447
shrvya <shrivyashetty@gmail.com>
2021-09-13 20:23:56 +0530

    insertnode

---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,26 @@
+package binaryseach;
+
+
+public class MyBinaryTreeTest {
+
+	public static void main(String[] args) {
+		MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		System.out.println("size of tree is "+myBinaryTree.getSize());
+		myBinaryTree.getNodes();
+
+	}
+
+}
